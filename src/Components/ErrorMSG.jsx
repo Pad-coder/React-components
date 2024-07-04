90e76e6d8c3c496785075b0da016f06d5fdf96bb
Padmanaban <padmanaban870@gmail.com>
2024-07-04 23:06:04 +0530

    React reusable components

---
+++ src/Components/ErrorMSG.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Error(){
+    return<>
+    <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
+    </>
+}
+export default Error
