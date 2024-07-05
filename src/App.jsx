ce91c08006c9c1be6559cc26d1ba47925630f349
Padmanaban <padmanaban870@gmail.com>
2024-07-05 15:58:19 +0530

    Components spilited

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import NavBar from './Components/NavBar'
-import Header from './Components/Header'
-import IconGrid from './Components/IconGrid'
-import Showcase from './Components/ShowCase'
-import Testimonial from './Components/Testimonial'
-import SignUp from './Components/SignUp'
-import Footer from './Components/Footer'
-
+import React from "react";
+import NavBar from "./Components/NavBar";
+import Header from "./Components/Header";
+import IconGrid from "./Components/IconGrid";
+import Showcase from "./Components/ShowCase";
+import Testimonial from "./Components/Testimonial";
+import SignUp from "./Components/SignUp";
+import Footer from "./Components/Footer";
 
 function App() {
-  return <>
-  <NavBar />
-  <Header />
-  <IconGrid />
-  <Showcase />
-  <Testimonial />
-  <SignUp />
-  <Footer />
-  </>
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <IconGrid />
+      <Showcase />
+      <Testimonial />
+      <SignUp />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -3,22 +3,28 @@ import Email from "./EmailInput";
 import Success from "./SubmitSuccess";
 import Error from "./ErrorMSG";
 
-function Form(){
-    return<>
-    <form className="form-subscribe" id="contactForm" data-sb-form-api-token="8d432adc-e48b-4a32-8ba1-d12b5fa8c1a4">
-                                {/* <!-- Email address input--> */}
-                                <Email />
-                                {/* <!-- Submit success message-->
+function Form() {
+  return (
+    <>
+      <form
+        className="form-subscribe"
+        id="contactForm"
+        data-sb-form-api-token="8d432adc-e48b-4a32-8ba1-d12b5fa8c1a4"
+      >
+        {/* <!-- Email address input--> */}
+        <Email />
+        {/* <!-- Submit success message-->
                                 <!---->
                                 <!-- This is what your users will see when the form-->
                                 <!-- has successfully submitted--> */}
-                               <Success />
-                                {/* <!-- Submit error message-->
+        <Success />
+        {/* <!-- Submit error message-->
                                 <!---->
                                 <!-- This is what your users will see when there is-->
                                 <!-- an error submitting the form--> */}
-                                <Error/>
-                            </form>
+        <Error />
+      </form>
     </>
+  );
 }
 export default Form;
